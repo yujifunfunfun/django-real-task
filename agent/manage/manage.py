61bc73449ca386c8f8ede5d365963cc93dcda528
Tozuka Yuji <yuji_tozuka@ydktechs.co.jp>
2021-06-30 16:26:06 +0900

    commit

--- agent/manage/manage.py
+++ agent/manage/manage.py
@@ -1,16 +1,31 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from common.database import SessionLocal
 from agent.models.syuppin_item import *
 from agent.engine.scraping import *
+from agent.models.url_list import *
 
-class Manage():
 
-    def add_db(self,url):
 
-        s = ScrapingMercari()
-        scraping_data = s.scraping_mercari(url)
+def add_db():
+    
+    s = ScrapingMercari()
+    db = SessionLocal()
 
-        db:SessionLocal() # DBセッションをオープン
-        item = SyuppinItem(item_name=scraping_data.item_name,price=scraping_data.price,) # 格納する商品レコードを作成
-        db.query(item) # DBに追加 
+    # urls = UrlList.objects.values_list('url_list')
+    urls = db.query(UrlList).all()
+
+    for url in urls:
+        scraping_data = s.scraping_mercari(url.url_list)
+
+        item = SyuppinItem(item_name=scraping_data.item_name,price=scraping_data.price,seller_name=scraping_data.seller_name,image_url1=scraping_data.image_urls,url=scraping_data.url,site=scraping_data.site) # 格納する商品レコードを作成
+        db.add(item) # DBに追加 
         db.commit() # 変更を確定
-        db.close() # DBをクローズ
+
+    db.close() # DBをクローズ
+    print('ok')
+
+
+if __name__ == "__main__":
+    add_db()
